fcfb8de0c348c905283610095b3ba6b1ecdbc511
Georgiana Ojoc <gojoc@deloittece.com>
2021-12-16 20:21:58 +0200

    Added vet index page and controller (closes #13)

---
+++ PetClinicWeb/src/main/java/udemy/spring/petclinic/controller/VetController.java
@@ -0,0 +1,14 @@
+package udemy.spring.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("vets")
+public class VetController {
+    @GetMapping({"", "/", "index", "index.html"})
+    public String getOwners() {
+        return "vet/index";
+    }
+}
