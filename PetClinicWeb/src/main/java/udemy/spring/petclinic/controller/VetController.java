0f9818f2d4f45863d5ff0739c74f8bfc7df886d8
Georgiana Ojoc <gojoc@deloittece.com>
2021-12-20 14:37:00 +0200

    Listed all vets (closes #16)

--- PetClinicWeb/src/main/java/udemy/spring/petclinic/controller/VetController.java
+++ PetClinicWeb/src/main/java/udemy/spring/petclinic/controller/VetController.java
@@ -1,14 +1,21 @@
 package udemy.spring.petclinic.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.spring.petclinic.service.VetService;
 
 @Controller
 @RequestMapping("vets")
+@AllArgsConstructor
 public class VetController {
+    private final VetService vetService;
+
     @GetMapping({"", "/", "index", "index.html"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
