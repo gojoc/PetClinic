a7602ceaa7b0520cfa908e04d26e7076d44322f5
Georgiana Ojoc <gojoc@deloittece.com>
2021-12-20 14:36:46 +0200

    Listed all owners (closes #15)

--- PetClinicWeb/src/main/java/udemy/spring/petclinic/controller/OwnerController.java
+++ PetClinicWeb/src/main/java/udemy/spring/petclinic/controller/OwnerController.java
@@ -1,14 +1,21 @@
 package udemy.spring.petclinic.controller;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import udemy.spring.petclinic.service.OwnerService;
 
 @Controller
 @RequestMapping("owners")
+@AllArgsConstructor
 public class OwnerController {
+    private final OwnerService ownerService;
+
     @GetMapping({"", "/", "index", "index.html"})
-    public String getOwners() {
+    public String getOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owner/index";
     }
 }
