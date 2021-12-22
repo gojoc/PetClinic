f52cf1ce19dd7a777463e4eff41539d9c49bd793
Georgiana Ojoc <gojoc@deloittece.com>
2021-12-22 12:44:14 +0200

    Added Visit entity (closes #25)

--- PetClinicData/src/main/java/udemy/spring/petclinic/model/Owner.java
+++ PetClinicData/src/main/java/udemy/spring/petclinic/model/Owner.java
@@ -1,12 +1,19 @@
 package udemy.spring.petclinic.model;
 
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Set;
 import java.util.UUID;
 
+@Getter
+@Setter
 @ToString(callSuper = true)
 public class Owner extends Person {
+    private Set<Pet> pets;
+
     @Builder
     public Owner(UUID id, String firstName, String lastName) {
         setId(id);
