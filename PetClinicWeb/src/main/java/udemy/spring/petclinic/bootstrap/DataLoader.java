100889d093da9378ed18410048af251941184896
Georgiana Ojoc <gojoc@deloittece.com>
2021-12-22 13:06:35 +0200

    Added pet types at startup (closes #31)

--- PetClinicWeb/src/main/java/udemy/spring/petclinic/bootstrap/DataLoader.java
+++ PetClinicWeb/src/main/java/udemy/spring/petclinic/bootstrap/DataLoader.java
@@ -6,19 +6,34 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import udemy.spring.petclinic.exception.NullException;
 import udemy.spring.petclinic.model.Owner;
+import udemy.spring.petclinic.model.PetType;
 import udemy.spring.petclinic.model.Vet;
 import udemy.spring.petclinic.service.OwnerService;
+import udemy.spring.petclinic.service.PetTypeService;
 import udemy.spring.petclinic.service.VetService;
 
 @Slf4j
 @Component
 @AllArgsConstructor
 public class DataLoader implements CommandLineRunner {
+    private final PetTypeService petTypeService;
     private final OwnerService ownerService;
     private final VetService vetService;
 
     @Override
     public void run(String... arguments) throws NullException {
+        PetType firstPetType = PetType.builder()
+                .name("Dog")
+                .build();
+        PetType firstSavedPetType = petTypeService.save(firstPetType);
+        log.info("[DataLoader] saved firstPetType: {}", firstSavedPetType);
+
+        PetType secondPetType = PetType.builder()
+                .name("Cat")
+                .build();
+        PetType secondSavedPetType = petTypeService.save(secondPetType);
+        log.info("[DataLoader] saved secondPetType: {}", secondSavedPetType);
+
         Owner firstOwner = Owner.builder()
                 .firstName("Jane")
                 .lastName("Austen")
